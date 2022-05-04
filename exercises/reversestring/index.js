e22e67756719b6b673fab09a25a98f8c1c4be53a
justinctollison <justinctollison@gmail.com>
2022-05-04 14:59:08 -0700

    Completed palindrome assignment.

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -8,12 +8,28 @@
 
 function reverse(str) {
 
-    let reversed = '';
+    return str.split('').reduce((reversed, character) => {
+        return character + reversed
+    }, '')
+    
+    //____________________________________________________
+    // let reversed = ''
 
-    for ( let i = str.length - 1; i >= 0; i--) {
-        reversed += str[i]
-    }
-    return reversed
+    // for (let character of str) {
+    //     reversed = character + reversed
+    // }
+    // return reversed
+
+    // return str.split('').reverse().join('')
+
+    //______________________________________________________________
+
+//     let reversed = '';
+
+//     for ( let i = str.length - 1; i >= 0; i--) {
+//         reversed += str[i]
+//     }
+//     return reversed
 }
 
 module.exports = reverse;
